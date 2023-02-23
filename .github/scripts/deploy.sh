0ce8c105ab9e676b6932b0307b9ac537ddd769be
Nick Tampakas <nick@gmx.co.uk>
2023-02-23 20:31:55 +0200

    Fix workflow

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -x
  
 commit_hash=$(git rev-parse HEAD)
 DEPLOY_ID=$(aws deploy create-deployment --application-name drill --deployment-group-name drill --github-location repository=$GITHUB_REPOSITORY,commitId=$commit_hash --ignore-application-stop-failures --file-exists OVERWRITE --output text)
 
 while true; do
   STATUS=$(aws deploy get-deployment --deployment-id $DEPLOY_ID --query 'deploymentInfo.status' --output text)
-  if [ $STATUS != "InProgress" ]; then
+  if [ $STATUS != "InProgress" ] && [ $STATUS != "Created" ]; then
     if [ $STATUS = "Succeeded" ]; then
        echo "SUCCESS"
        exit 0
