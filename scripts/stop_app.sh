f1b157994097d4e343613316973a58d4d95a5e4c
Nick Tampakas <nick@gmx.co.uk>
2023-03-15 09:43:55 +0200

    Version 27

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -3,6 +3,8 @@
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 docker pull 490752553772.dkr.ecr.eu-central-1.amazonaws.com/zkgroups:latest
 
+sleep 1
+
 docker stop app1
 
 exit 0
